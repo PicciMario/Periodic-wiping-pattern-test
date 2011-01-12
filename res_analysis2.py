0b649c5c6d72230731b5276eb683573cdad51d3e
PicciMario <mario.piccinelli@gmail.com>
2011-01-12 21:38:11 +0100

    various

--- res_analysis2.py
+++ res_analysis2.py
@@ -35,7 +35,9 @@ for o, a in opts:
 
 # reading file
 
-print "\nAnalyzing file " + filename + "\n";
+print "\nAnalyzing file " + filename;
+
+print "\nDistribution of the percentage of FFT values over 95% in each run (should be around 5 for random data, and around 0 for periodic data).";
 
 in_file = open(filename,"r")
 
@@ -52,15 +54,26 @@ for line in in_file:
 tot = sum(bin);
 
 if (tot != 0):
+
+	print "\nFound " + str(tot) + " runs in file.";
 	
 	for i in range(len(bin)):
 		bin_perc[i] = float(bin[i]) / float(tot) * 100;
+		
+	if ((bin_perc[0]+bin_perc[1]) > 50):
+		print "\nFile seems PERIODICAL (over 50% of tests with less than 2% peaks in the FFT higher than 95% of expected value)"
+	else:
+		print "\nFile doesn't seem periodical";
+	
+	print "";
 	
 	for i in range(len(bin_perc)):
-		string = str(i).zfill(2) + " - " + ("%3.2f" % bin_perc[i]).zfill(5) + " ";
+		string = str(i).zfill(2) + " - " + ("%3.2f" % bin_perc[i]).zfill(5) + "%  ";
 		for rep in range(int(bin_perc[i])):
 			string = string + "*";
 		print string;
+	
+	print "\nWhere the column 0 contains results from 0.00 to 0.9999 and so on."
 	
 else:
 	print "No results.\n";
